a96fa96868580083a03add6e192127fb66f25d8f
Sam LaGrave <srl0011@uah.edu>
2020-11-03 17:44:51 -0600

    More logic

--- generate.py
+++ generate.py
@@ -1,19 +1,23 @@
 """
 Author:         Sam LaGrave (github.com/SLaGrave)
 Date:           11/01/2020
-Last Updated:   ---
+Last Updated:   11/03/2020
 Purpose:        To generate 50k words of text for NaNoGenMo2020
 """
+
+from utils import *
+
 import random
-from pycorpora import plants, colors
+
+from pycorpora import plants, colors, animals
+
 
 # I want the output to be in a nice format. In the future I will likely try
 # to use a pdf library, or possibly output LaTeX that can generate nice
 # documents.
 
 # Document constants
-HEADER = """
-Whimsical Forest Field Guide
+HEADER = """Whimsical Forest Field Guide
 SLaGrave (and his code)
 
 A field guide to help you survive a magical, whimsical forest.\n\n
@@ -27,9 +31,28 @@ for p in zip(random_colors, random_flowers):
     pair = p[0] + " " + p[1]
     flora_test.append(pair)
 
+random_animals = random.sample(animals.common["animals"], 10)
+random_colors = random.sample([item['color'] for item in colors.crayola["colors"]], 10)
+fauna_test = list()
+for p in zip(random_colors, random_animals):
+    pair = p[0] + " " + p[1]
+    fauna_test.append(pair)
+
+Flora_TOC = [f"\t\t{pair}\n" for pair in flora_test]
+Fauna_TOC = [f"\t\t{pair}\n" for pair in fauna_test]
+
 # Writes the actual html document
 with open("output.txt", "w") as f:
     f.writelines(HEADER)
 
-word_count = len(HEADER.split())
-print(f"Word Count: {word_count}")
+    f.writelines("Table of Contents:\n")
+    f.writelines("\tFlora\n")
+    for line in Flora_TOC:
+        f.writelines(line)
+    f.writelines("\tFauna\n")
+    for line in Fauna_TOC:
+        f.writelines(line)
+
+with open("output.txt", "r") as f:
+    data = f.read()
+print(f"Word Count: {len(data.split())}")
---
+++ utils.py
@@ -0,0 +1,14 @@
+"""
+Author:         Sam LaGrave (github.com/SLaGrave)
+Date:           11/03/2020
+Last Updated:   ---
+Purpose:        Helper functions used in generate.py
+"""
+
+def entry_for_flora(name):
+    # TODO: Add logic
+    pass
+
+def entry_for_fauna(name):
+    # TODO: Add logic
+    pass
